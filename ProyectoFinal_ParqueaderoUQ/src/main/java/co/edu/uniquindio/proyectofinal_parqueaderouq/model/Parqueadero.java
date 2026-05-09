a58a971a7bb8adc25a18f05486459346021916cc
santiagoduquem-del <santiago.duquem@uqvirtual.edu.co>
2026-05-08 23:46:57 -0500

    clase Parqueadero

--- ProyectoFinal_ParqueaderoUQ/src/main/java/co/edu/uniquindio/proyectofinal_parqueaderouq/model/Parqueadero.java
+++ ProyectoFinal_ParqueaderoUQ/src/main/java/co/edu/uniquindio/proyectofinal_parqueaderouq/model/Parqueadero.java
@@ -1,4 +1,39 @@
 package co.edu.uniquindio.proyectofinal_parqueaderouq.model;
 
 public class Parqueadero {
+
+    private String nombre;
+    private String Direccion;
+    private String nivel;
+
+
+    public Parqueadero(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.Direccion = direccion;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Parqueadero{" +
+                "nombre='" + nombre + '\'' +
+                ", Direccion='" + Direccion + '\'' +
+                '}';
+    }
 }
