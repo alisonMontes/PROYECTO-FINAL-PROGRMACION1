63588552e7a6e05ee06364594669b9aeab4372e1
alisonMontes <alisonf.montesm@uqvirtual.edu.co>
2026-05-08 22:40:18 -0500

    Add files via upload

---
+++ ProyectoFinal_ParqueaderoUQ/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.proyectofinal_parqueaderouq {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.proyectofinal_parqueaderouq to javafx.fxml;
+    exports co.edu.uniquindio.proyectofinal_parqueaderouq;
+}
