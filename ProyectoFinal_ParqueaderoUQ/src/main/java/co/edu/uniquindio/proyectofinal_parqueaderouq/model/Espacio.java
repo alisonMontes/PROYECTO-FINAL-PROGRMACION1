e3c9768a857e375748684f921f51a9ef9588c897
santiagoduquem-del <santiago.duquem@uqvirtual.edu.co>
2026-05-14 11:42:27 -0500

    Correcion clase espacio y carga del diagrama UML
    Diagrama UML sujeto a correcciones

--- ProyectoFinal_ParqueaderoUQ/src/main/java/co/edu/uniquindio/proyectofinal_parqueaderouq/model/Espacio.java
+++ ProyectoFinal_ParqueaderoUQ/src/main/java/co/edu/uniquindio/proyectofinal_parqueaderouq/model/Espacio.java
@@ -1,23 +1,19 @@
 package co.edu.uniquindio.proyectofinal_parqueaderouq.model;
 
 import java.io.Serializable;
+public class Espacio{
 
-public class Espacio implements Serializable {
-    private static final long serialVersionUID = 1L;
     private String codigo;
     private tipoEspacio tipo;
     private estadoEspacio estado;
     private Vehiculo vehiculoAsignado;
+
 
     public Espacio(String codigo, tipoEspacio tipo, estadoEspacio estado, Vehiculo vehiculoAsignado) {
         this.codigo = codigo;
         this.tipo = tipo;
         this.estado = estado;
         this.vehiculoAsignado = vehiculoAsignado;
-    }
-
-    public Espacio() {
-        this.vehiculoAsignado = null;
     }
 
     public String getCodigo() {
